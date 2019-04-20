2c9d544fa53c1b9dcc96e2270dffc2f91990dd55
AndrewDettor <AndrewDettor@users.noreply.github.com>
2019-04-19 22:22:31 -0400

    turn pull_parts.py and part-page-parser.py into classes

--- part-page-parser.py
+++ part-page-parser.py
@@ -5,42 +5,91 @@ from bs4 import BeautifulSoup
 import ssl
 
 
-# Ignore SSL certificate errors
+class PartPageParser():
+    # LIST FORMAT: ('name', 'score', 'price')
+    # 'price' could equal 'NA'
 
-context = ssl.create_default_context()
-context.check_hostname = False
-context.verify_mode = ssl.CERT_NONE
+    def getGPUlist(self):
+        # Ignore SSL certificate errors
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
 
+        # Get all text from the Passmark GPU page, slim down to only GPUs
 
-# LIST FORMAT: ('name', 'score', 'price')
-# 'price' could equal 'NA'
+        url = 'https://www.videocardbenchmark.net/common_gpus.html'
 
+        html = urllib.request.urlopen(url, context=context).read()
+        soup = BeautifulSoup(html, 'html.parser')
 
-def getGPUlist():
-    # Get all text from the Passmark GPU page, slim down to only GPUs
+        GPUlist = soup.get_text().split('\n')
+        GPUlist = GPUlist[182: 282]
 
-    url = 'https://www.videocardbenchmark.net/common_gpus.html'
+        newGPUlist = []
 
-    html = urllib.request.urlopen(url, context=context).read()
-    soup = BeautifulSoup(html, 'html.parser')
+        for index, value in enumerate(GPUlist):
+            if index % 2 == 0 or index == 0:
 
-    GPUlist = soup.get_text().split('\n')
-    GPUlist = GPUlist[182: 282]
+                # Name and Score are on every even index. Split by space, and remove empty elements.
+                templist = value.strip().split(" ")
+                templist = list(filter(None, templist))
 
-    newGPUlist = []
+                # Score is the last element.
+                tempscore = templist[-1]
 
-    for index, value in enumerate(GPUlist):
-        if index % 2 is 0 or index is 0:
+                # Shorten templist down to just the name of the part.
+                templist = templist[:-1]
 
-            # Name and Score are on every even index. Split by space, and remove empty elements.
+                #  Name was split by each space, so put it back together
+                tempname = ''
+                for value in templist:
+                    tempname = tempname + value + ' '
+
+                # Remove the extra space
+                tempname = tempname[:-1]
+
+                # Price is on the next index after the Name/Score index. Get price, then remove the *.
+                tempprice = GPUlist[index + 1]
+                if '*' in tempprice:
+                    tempprice = tempprice[:-1]
+
+                # Put tuple of info in new GPUlist bc the indexes are fucked on the original list
+                newGPUlist.append((tempname, tempscore, tempprice))
+
+        return newGPUlist
+
+    def getCPUlist(self):
+        # Ignore SSL certificate errors
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+
+        # Get all text from the Passmark CPU page, slim down to only CPUs
+
+        url = 'https://www.cpubenchmark.net/common_cpus.html'
+
+        html = urllib.request.urlopen(url, context=context).read()
+        soup = BeautifulSoup(html, 'html.parser')
+
+        CPUlist = soup.get_text().split('\n')
+        CPUlist = CPUlist[198: 298]
+
+        for index, value in enumerate(CPUlist):
+
+            # Each value has all the info in one index. Split each index by space, and remove empty space.
             templist = value.strip().split(" ")
             templist = list(filter(None, templist))
 
-            # Score is the last element.
-            tempscore = templist[-1]
+            # Price is the last element. Get price, then remove the * if there is one
+            tempprice = templist[-1]
+            if '*' in tempprice:
+                tempprice = tempprice[:-1]
+
+            # Score is the second to last element
+            tempscore = templist[-2]
 
             # Shorten templist down to just the name of the part.
-            templist = templist[:-1]
+            templist = templist[:-2]
 
             #  Name was split by each space, so put it back together
             tempname = ''
@@ -50,101 +99,59 @@ def getGPUlist():
             # Remove the extra space
             tempname = tempname[:-1]
 
-            # Price is on the next index after the Name/Score index. Get price, then remove the *.
-            tempprice = GPUlist[index + 1]
-            if '*' in tempprice:
-                tempprice = tempprice[:-1]
+            # Replace the original spot in CPUlist with the tuple of info
+            CPUlist[index] = (tempname, tempscore, tempprice)
 
-            # Put tuple of info in new GPUlist bc the indexes are fucked on the original list
-            newGPUlist.append((tempname, tempscore, tempprice))
+        return CPUlist
 
-    return newGPUlist
+    def getDrivelist(self):
+        # Ignore SSL certificate errors
+        context = ssl.create_default_context()
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
 
+        # Get all text from the Passmark Hard Drive page, slim down to only Hard Drives
 
-def getCPUlist():
-    # Get all text from the Passmark CPU page, slim down to only CPUs
+        url = 'https://www.harddrivebenchmark.net/common_drives.html'
 
-    url = 'https://www.cpubenchmark.net/common_cpus.html'
+        html = urllib.request.urlopen(url, context=context).read()
+        soup = BeautifulSoup(html, 'html.parser')
 
-    html = urllib.request.urlopen(url, context=context).read()
-    soup = BeautifulSoup(html, 'html.parser')
+        drivelist = soup.get_text().split('\n')
+        drivelist = drivelist[183:483]
 
-    CPUlist = soup.get_text().split('\n')
-    CPUlist = CPUlist[198: 298]
+        newdrivelist = []
+        count = 3
 
-    for index, value in enumerate(CPUlist):
+        for index, value in enumerate(drivelist):
+            # Each 3 elements in drivelist need to be combined into one tuple
+            if count % 3 == 0:
+                tempname = drivelist[index]
+                tempscore = drivelist[index + 1]
+                tempprice = drivelist[index + 2]
 
-        # Each value has all the info in one index. Split each index by space, and remove empty space.
-        templist = value.strip().split(" ")
-        templist = list(filter(None, templist))
+                if '*' in tempprice:
+                    tempprice = tempprice[:-1]
 
-        # Price is the last element. Get price, then remove the * if there is one
-        tempprice = templist[-1]
-        if '*' in tempprice:
-            tempprice = tempprice[:-1]
+                newdrivelist.append((tempname, tempscore, tempprice))
 
-        # Score is the second to last element
-        tempscore = templist[-2]
+            count += 1
 
-        # Shorten templist down to just the name of the part.
-        templist = templist[:-2]
+        return newdrivelist
 
-        #  Name was split by each space, so put it back together
-        tempname = ''
-        for value in templist:
-            tempname = tempname + value + ' '
-
-        # Remove the extra space
-        tempname = tempname[:-1]
-
-        # Replace the original spot in CPUlist with the tuple of info
-        CPUlist[index] = (tempname, tempscore, tempprice)
-
-    return CPUlist
+    def getRAMlist(self):
+        # FORMAT [('name', 'type', 'speed', 'size', 'price/GB', 'price')]
+        return [('G.Skill NT Series', 'DDR4', '2133 Mhz', '8 GB', '$5.50', '$43.98'),
+                ('Team Vulcan', 'DDR4', '2400 Mhz', '8 GB', '$5.62', '$44.99'),
+                ('Crucial Ballistix Sport LT', 'DDR4', '2666 Mhz', '8 GB', '$5.74', '$45.89')]
 
 
-def getDrivelist():
-    # Get all text from the Passmark Hard Drive page, slim down to only Hard Drives
-
-    url = 'https://www.harddrivebenchmark.net/common_drives.html'
-
-    html = urllib.request.urlopen(url, context=context).read()
-    soup = BeautifulSoup(html, 'html.parser')
-
-    drivelist = soup.get_text().split('\n')
-    drivelist = drivelist[183:483]
-
-    newdrivelist = []
-    count = 3
-
-    for index, value in enumerate(drivelist):
-        # Each 3 elements in drivelist need to be combined into one tuple
-        if count % 3 is 0:
-            tempname = drivelist[index]
-            tempscore = drivelist[index + 1]
-            tempprice = drivelist[index + 2]
-
-            if '*' in tempprice:
-                tempprice = tempprice[:-1]
-
-            newdrivelist.append((tempname, tempscore, tempprice))
-
-        count += 1
-
-    return newdrivelist
-
-
-def getRAMlist():
-    # FORMAT [('name', 'type', 'speed', 'size', 'price/GB', 'price')]
-    return [('G.Skill NT Series', 'DDR4', '2133 Mhz', '8 GB', '$5.50', '$43.98'),
-            ('Team Vulcan', 'DDR4', '2400 Mhz', '8 GB', '$5.62', '$44.99'),
-            ('Crucial Ballistix Sport LT', 'DDR4', '2666 Mhz', '8 GB', '$5.74', '$45.89')]
-
-
-print(getGPUlist())
+# test
+ppp = PartPageParser()
+print(ppp.getGPUlist())
 print('\n\n')
-print(getCPUlist())
+print(ppp.getCPUlist())
 print('\n\n')
-print(getDrivelist())
+print(ppp.getDrivelist())
 print('\n\n')
-print(getRAMlist())
+print(ppp.getRAMlist())
--- pull_parts.py
+++ pull_parts.py
@@ -1,37 +1,36 @@
 import subprocess
 
 
-def getGPUname():
-    get_gpu_info = "wmic path win32_VideoController get name"
-    return subprocess.call(get_gpu_info, shell=True)
+class PullParts():
+    def getGPUname(self):
+        get_gpu_info = "wmic path win32_VideoController get name"
+        return subprocess.call(get_gpu_info, shell=True)
+
+    def getCPUname(self):
+        get_cpu_info = "wmic cpu get name"
+        return subprocess.call(get_cpu_info, shell=True)
+
+    def getRAMinfo(self):
+        get_RAM_info = "wmic MEMORYCHIP get BankLabel, DeviceLocator, MemoryType, TypeDetail, Capacity, Speed"
+        return subprocess.call(get_RAM_info, shell=True)
+
+    def getDriveCapacity(self):
+        get_drive_capacity = "wmic logicaldisk get size,freespace,caption"
+        return subprocess.call(get_drive_capacity, shell=True)
+
+    def getDrivenames(self):
+        get_drive_names = "wmic diskdrive get caption"
+        return subprocess.call(get_drive_names, shell=True)
 
 
-def getCPUname():
-    get_cpu_info = "wmic cpu get name"
-    return subprocess.call(get_cpu_info, shell=True)
-
-
-def getRAMinfo():
-    get_RAM_info = "wmic MEMORYCHIP get BankLabel, DeviceLocator, MemoryType, TypeDetail, Capacity, Speed"
-    return subprocess.call(get_RAM_info, shell=True)
-
-
-def getDriveCapacity():
-    get_drive_capacity = "wmic logicaldisk get size,freespace,caption"
-    return subprocess.call(get_drive_capacity, shell=True)
-
-
-def getDrivenames():
-    get_drive_names = "wmic diskdrive get caption"
-    return subprocess.call(get_drive_names, shell=True)
-
-
-print(getGPUname())
+# test
+pp = PullParts()
+print(pp.getGPUname())
 print('\n\n')
-print(getCPUname())
+print(pp.getCPUname())
 print('\n\n')
-print(getRAMinfo())
+print(pp.getRAMinfo())
 print('\n\n')
-print(getDriveCapacity())
+print(pp.getDriveCapacity())
 print('\n\n')
-print(getDrivenames())
+print(pp.getDrivenames())
