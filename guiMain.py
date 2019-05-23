739e3a1eef34a4b9e65e27c357d447195d882148
AndrewDettor <AndrewDettor@users.noreply.github.com>
2019-05-23 11:48:54 -0400

    fix text resizing bug, adjust ui for new sizes

--- guiMain.py
+++ guiMain.py
@@ -43,7 +43,12 @@ class MainPage(QDialog):
             scoresList = [scoreCPU, score2D, score3D, scoreMemory, scoreDisk, scorePassmark]
             # IMPLEMENT INPUT CHECKING HERE
             # assuming inputs are good...
-            self.textEdit_8.setText("Scores Entered Correctly? - YES")
+            self.textEdit_8.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+            "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+            "p, li { white-space: pre-wrap; }\n"
+            "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+            "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">Scores Entered Correctly? - YES</span></p></body></html>")
+
             intScoresList = [int(score) for score in scoresList]
             # Quantify self.performance
             PassMark = intScoresList[5]
@@ -57,9 +62,23 @@ class MainPage(QDialog):
                 self.performance = 4
 
             # change text for question 1
-            self.textEdit_9.setText("What do you wish to use your computer for?")
-            self.textEdit_10.setText("Waiting on choice for first question...")
-            self.textEdit_11.setText("Final Verdict: Waiting on choice for first question...")
+            self.textEdit_9.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+            "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+            "p, li { white-space: pre-wrap; }\n"
+            "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+            "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">What do you wish to use your computer for?</span></p></body></html>")
+
+            self.textEdit_10.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+            "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+            "p, li { white-space: pre-wrap; }\n"
+            "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+            "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Waiting on choice for first question...</span></p></body></html>")
+
+            self.textEdit_11.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+            "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+            "p, li { white-space: pre-wrap; }\n"
+            "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+            "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">Final Verdict: Waiting on choice for first question...</span></p></body></html>")
 
             # fill the comboBox for the question 1
             options = ["Web browsing / Office", "Video Games", "Workstation"]
@@ -79,23 +98,43 @@ class MainPage(QDialog):
                 self.use = 3
 
             # change text of verdict box
-            self.textEdit_11.setText("Final Verdict: Waiting on choice for second question...")
+
+            self.textEdit_11.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+            "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+            "p, li { white-space: pre-wrap; }\n"
+            "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+            "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">Final Verdict: Waiting on choice for second question...</span></p></body></html>")
 
             if self.use == 1:
                 self.submitQuestion2()
-                self.textEdit_10.setText("")
+
+                self.textEdit_10.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                "p, li { white-space: pre-wrap; }\n"
+                "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\"></span></p></body></html>")
 
             elif self.use == 2:
-                self.textEdit_10.setText("At medium settings, what framerate do you hope to achieve?")
+                self.textEdit_10.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                "p, li { white-space: pre-wrap; }\n"
+                "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">At medium settings, what framerate do you hope to achieve?</span></p></body></html>")
+
                 # fill the comboBox for question 2
                 options = ["30 fps", "60 fps", "144 fps"]
                 for option in options:
                     self.comboBox_2.addItem(option)
 
             elif self.use == 3:
-                self.textEdit_10.setText("What do you use your workstation for?")
+                self.textEdit_10.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                "p, li { white-space: pre-wrap; }\n"
+                "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">What do you use your workstation for?</span></p></body></html>")
+
                 # fill the comboBox for question 2
-                options = ["Rendering 1080p Footage", "Rendering 4K Footage", "AutoDesk Applications", "VMs", "Listen bro, I just work here and they give me this hunk of metal"]
+                options = ["Rendering 1080p Footage", "Rendering 4K Footage", "AutoDesk Applications", "VMs", "No clue"]
                 for option in options:
                     self.comboBox_2.addItem(option)
 
@@ -109,13 +148,23 @@ class MainPage(QDialog):
             if self.use == 2 and choice2 != "30 fps":
                 self.use = 3
 
-            if self.use == 3 and choice2 != "Listen bro, I just work here and they give me this hunk of metal":
+            if self.use == 3 and choice2 != "No clue":
                 self.use = 4
 
             if self.performance >= self.use:
-                self.textEdit_11.setText("Final Verdict: Your computer is capable of the tasks you require of it.")
+                self.textEdit_11.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                "p, li { white-space: pre-wrap; }\n"
+                "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">Final Verdict: Your computer is capable of the tasks you require of it.</span></p></body></html>")
+
             else:
-                self.textEdit_11.setText("Final Verdict: Your computer should be upgraded to more adequately fufill the tasks required of it.")
+
+                self.textEdit_11.setHtml("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+                "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+                "p, li { white-space: pre-wrap; }\n"
+                "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:7.8pt; font-weight:400; font-style:normal;\">\n"
+                "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">Final Verdict: Your computer should be upgraded to more adequately fufill the tasks required of it.</span></p></body></html>")
 
             # this is where it would call the other parts of the program
 
